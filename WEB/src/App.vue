42d61e6bd01b8ab19f2f1d138be3f9dec8942131
Rayan <rayansiqueira@gmail.com>
2024-01-21 09:50:08 -0300

    Fix: show shortlink

--- WEB/src/App.vue
+++ WEB/src/App.vue
@@ -3,22 +3,42 @@
     <div class="flex flex-col gap-8">
       <div>
         <h1 class="text-white text-5xl font-bold">Encurtador de links</h1>
-        <span class="text-zinc-500 mt-2"
-          >Reduza o tamanho dos seus links para conferir-lhes uma aparência mais elegante!</span
-        >
+        <span class="text-zinc-500 mt-2">Reduza o tamanho dos seus links para conferir-lhes uma aparência mais
+          elegante!</span>
       </div>
       <div class="flex gap-2">
-        <input
-          v-model="linkInput" 
-          @keyup.enter="encurtarLink"
-          type="text"
+        <input 
+          type="text" 
           placeholder="Insira seu link"
+          v-model="linkInput"
           class="p-2 w-full bg-transparent rounded-md border border-zinc-800 outline-none text-white focus:outline-violet-600"
-        />
-        <button @click="encurtarLink" class="bg-violet-800 p-2 rounded-lg hover:bg-violet-600 duration-100">
+          @keyup.enter="encurtarLink" />
+
+        <button @click="encurtarLink" :disabled="isLoading"
+          class="bg-violet-800 p-2 rounded-lg hover:bg-violet-600 duration-100">
           <Link class="text-white" />
         </button>
       </div>
+
+      <div v-if="showResult" class="w-full h-fit p-6 border border-zinc-800 rounded-lg flex flex-col gap-4">
+        <div class="flex flex-col">
+          <span class="text-zinc-500">Seu link original:</span>
+          <input 
+            readonly
+            type="text"
+            :value="linkOriginal"
+            class="p-2 w-full bg-transparent rounded-md border border-zinc-800 outline-none text-white focus:outline-violet-600" />
+        </div>
+        <div class="flex flex-col">
+          <span class="text-zinc-500">Seu link encurtado:</span>
+          <input 
+            readonly
+            type="text"
+            :value="apiResponse"
+            class="p-2 w-full bg-transparent rounded-md border border-zinc-800 outline-none text-white focus:outline-violet-600" />
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -26,10 +46,45 @@
 <script setup>
 import { ref } from "vue";
 import { Link } from "lucide-vue-next";
+import axios from "axios";
+import validator from 'validator';
 
 const linkInput = ref("");
+const linkOriginal = ref("");
+const isLoading = ref(false);
+const showResult = ref(false);
+const apiResponse = ref(null);
+const isInvalidLink = ref(false);
 
-const encurtarLink = () => {
-  console.log(linkInput.value);
+const isValidLink = (link) => {
+  return validator.isURL(link);
+};
+
+const encurtarLink = async () => {
+  if (isValidLink(linkInput.value)) {
+    isLoading.value = true;
+
+    try {
+      const response = await axios.post("http://localhost/api/encurtar-link", {
+        link: linkInput.value,
+      });
+      apiResponse.value = response.data.url_encurtada; 
+      showResult.value = true;
+      linkOriginal.value = linkInput.value; 
+      linkInput.value = "";
+      isInvalidLink.value = false;
+      console.log("Link encurtado com sucesso:", response.data);
+
+    } catch (error) {
+      isInvalidLink.value = true; 
+      console.error("Erro ao encurtar link:", error.message);
+
+    } finally {
+      isLoading.value = false;
+    }
+  } else {
+    isInvalidLink.value = true;
+    console.warn("Link inválido. Por favor, insira um link válido.");
+  }
 };
 </script>
---
+++ WEB/src/components/Input.vue
@@ -0,0 +1,37 @@
+<template>
+    <input
+      v-bind="$attrs"
+      :type="type"
+      :placeholder="placeholderText"
+      v-model="inputValue"
+      class="p-2 w-full bg-transparent rounded-md border border-zinc-800 outline-none text-white focus:outline-violet-600"
+    />
+  </template>
+  
+  <script>
+  export default {
+    name: "Input",
+    props: {
+      type: {
+        type: String,
+        default: "text",
+      },
+      placeholderText: {
+        type: String,
+        default: "",
+      },
+    },
+    data() {
+      return {
+        inputValue: "",
+      };
+    },
+    watch: {
+      inputValue(newValue) {
+        this.$emit("update:modelValue", newValue);
+        this.$emit("input", newValue);
+      },
+    },
+  };
+  </script>
+  
